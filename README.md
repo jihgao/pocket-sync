293ad24cb4997ded0862c7ef40b2b35158926fda
Jiheng Gao <love0.0chen@gmail.com>
2017-11-08 18:10:58 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # pocket-sync
+
 A command-line tool for synchronizing your pocket items to your local database
+
+## Install
+
+`npm install -g https://github.com/jihgao/pocket-sync.git`
+
+## Usage
+
+`pocketsync -k <consumer key> -t <access_token>`
+
+
+## Options
+
+* -k The consumer key of your pocket app
+
+* -t The access token of your pocket app
+
+## Notes
+
+You just need provide consumer key and access token at the first time you run the command
---
+++ index.js
@@ -0,0 +1,109 @@
+const request = require('request');
+const _ = require('lodash');
+const fs = require('fs');
+const ora = require('ora');
+const program = require('commander');
+const log = console.log;
+let cachedItems;
+let config;
+let optionError = false;
+
+if(!fs.existsSync('./db.json') ){
+  cachedItems = [];
+}else{
+  cachedItems = require('./db.json');
+}
+
+if(!fs.existsSync('./config.json') ){
+  config = {
+    page_size: 120,
+    last_page: 1
+  };
+}else{
+  config = require('./config.json');
+}
+
+if( !config.consumer_key || !config.access_token){
+    program
+   .version('0.0.1')
+   .option('-k, --key <consumer_key>', 'The consumer key of your pocket app')
+   .option('-t, --token <access_token>', 'The access token of your pocket app')
+   .parse(process.argv);
+
+   if( program.key ){
+      config.consumer_key = program.key;
+   }else{
+      optionError = true;
+      log('\x1B[31m', 'Please provide consumer key', '\x1B[39m');
+   }
+
+   if( program.token ) {
+      config.access_token = program.token;
+   }else{
+      optionError = true;
+      log('\x1B[31m', 'Please provide access token', '\x1B[39m');
+   }
+
+   if( optionError ){
+      process.exit(1);
+   }else{
+      fs.writeFileSync('config.json', JSON.stringify(config, null, 4));
+   }
+}
+
+const spinner = ora('Start sync').start();
+
+getItems(config.last_page);
+
+function getItems(){
+  let offset = (config.last_page - 1) * config.page_size + 1;
+  request.post({
+    url: 'https://getpocket.com/v3/get',
+    form: {
+      "consumer_key": config.consumer_key,
+      "access_token": config.access_token,
+      "state":"all",
+      "detailType":"simple",
+      "count": config.page_size,
+      "sort":"oldest",
+      "offset": offset,
+      "since": config.since || 0
+    }
+  }, function(error, response, body){
+    let result;
+    if( error ) {
+      log('\x1B[31m', error, config.page_size, offset, '\x1B[39m');
+      throw error;
+    }
+    try {
+        result = JSON.parse(body, null, 4);
+    } catch (e) {
+        result = null;
+    }
+
+    if( result ){
+      items = _.values(result.list);
+      if( items.length ){
+          [].push.apply(cachedItems, items);
+          config.last_page++
+          saveOptions();
+          spinner.text = 'synced ' + cachedItems.length;
+          getItems(config.last_page);
+      }else{
+          if( result.since ){
+            config.since = result.since;
+          }
+          config.last_page = 1;
+          saveOptions();
+          spinner.stop();
+          log("done! ");
+      }
+    }
+  });
+}
+
+function saveOptions(){
+  fs.writeFileSync('db.json', JSON.stringify(cachedItems, null, 4));
+  fs.writeFileSync('config.json', JSON.stringify(config, null, 4));
+}
+
