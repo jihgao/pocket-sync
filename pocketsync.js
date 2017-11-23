60fb0072b93a1d21da2658c6438bd8c98c165990
Jiheng Gao <love0.0chen@gmail.com>
2017-11-23 09:57:24 +0800

    update the version

--- pocketsync.js
+++ pocketsync.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const program = require('commander');
 program
-  .version('0.0.1')
+  .version('0.0.4')
   .description('Fake package manager')
   .command('reset', 'reset the access token and code')
   .command('export', 'export the synchronized data')
